3bee511abb8f1d0cdf0eae68db3bc4789df9345f
JeremyRoalef <roalef944@morrisville.edu>
2023-09-18 18:51:58 -0400

    Using FindObjectOfType

--- Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -7,22 +7,45 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float fltTorqueAmount = 1.0f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] float fltBaseSpeed = 12.5f;
+    [SerializeField] float fltBoostSpeed = 20f;
 
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
     {
+        RotatePlayer();
+
+        RespondToBoost();
+    }
+
+    void RotatePlayer()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb2d.AddTorque(fltTorqueAmount);        
+            rb2d.AddTorque(fltTorqueAmount);
         }
         else if (Input.GetKey(KeyCode.RightArrow)) //else prevents player from pressing both arrows at same time
         {
             rb2d.AddTorque(-fltTorqueAmount);
+        }
+    }
+    void RespondToBoost()
+    {
+        //if we push up, speed up. else, stay at normal speed
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = fltBoostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = fltBaseSpeed;
         }
     }
 }
