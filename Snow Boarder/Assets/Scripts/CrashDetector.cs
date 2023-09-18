82132e4ac9b9601fcde75f63d5a02ad80e22be91
JeremyRoalef <roalef944@morrisville.edu>
2023-09-18 14:07:32 -0400

    Using Invoke for Delays

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float fltReloadDelay = 1f;
+
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "Crash")
         {
-            SceneManager.LoadScene(0); //loads scene with index 0 in build settings.
+            Invoke("ReloadScene", fltReloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0); //loads scene with index 0 in build settings.
     }
 }
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -6,13 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float fltReloadDelay = 1f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
        if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0); //loads scene with index 0 in build settings.
+            Invoke("ReloadScene", fltReloadDelay);
         }
 
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0); //loads scene with index 0 in build settings.
     }
 }
